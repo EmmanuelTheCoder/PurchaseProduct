c95a571de9b501d6aa7bd2ebfcaf5c64600bd041
EmmanuelTheCoder <loyaltysamuel001@gmail.com>
2020-10-28 22:47:21 +0100

    changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,13 +13,23 @@ function Product(props) {
                     <Link to="/details">
                         <img src={img} alt="product" className="card-img-top"/>
                     </Link>
-                    <button className="cart-btn" 
+                    <CartButton className="cart-btn" 
                         disabled={inCart ? true : false}
                         onClick={()=>console.log("added to cart")}
                     >
                         {inCart ? (<p className="text-capitalize mb-0" disabled
                         >In cart</p>) : (<i className="fas fa-cart-plus" />)}
-                    </button>
+                    </CartButton>
+               </div>
+               {/* card footer */}
+               <div className="card-footer d-flex justify-content-between">
+                <p className="align-self-center wmb-0">
+                    {title}
+                </p>
+                <h5 className="text-blue font-italic mb-0">
+                    <span className="mr-1">$</span>
+                    {price}
+                </h5>
                </div>
            </div>
        </ProductWrapper>
@@ -28,5 +38,40 @@ function Product(props) {
 export default Product;
 
 const ProductWrapper = styled.div`
+    .card{
+        border-color: transparent;
+        transition: all 1s linear;
+    }
+    .card-footer{
+        background: transparent;
+        border-top: transparent;
+        transition: all 1s linear;
+    }
+    &:hover{
+        .card{
+            border: 0.04rem solid rgba(0,0,0,0.2);
+            box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2)
+        }
+        .card-footer{
+            background: rgba(247,247,247);
+        }
+    }
+    .img-container{
+        position: relative;
+        overflow: hidden;
+
+    }
+    .card-img-top{
+        transition: all 1s linear;
+    }
+    .img-container:hover .card-img-top{
+        transform: scale(1.2);
+    }
+`
+const CartButton = styled.button`
+    position: absolute;
+    bottom: .6rem;
+    right: .5rem;
+
 
 `
